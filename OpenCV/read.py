d96cd465020612976f2870fd178db7251cc04baa
Alef Luiz <59896850+AlefLuiz@users.noreply.github.com>
2021-10-16 19:55:07 -0300

    Junção da Front com Back

--- OpenCV/read.py
+++ OpenCV/read.py
@@ -3,34 +3,27 @@ from tkinter import filedialog
 
 import cv2 as cv
 import os
+import pathlib
 
-acesso = False
-root = tk.Tk()
-root.withdraw()
+class APIDigital():
+    
 
-dim = (1000, 1200)
+    def ProcurarDigital(self, userFile):
+        dim = (1000, 1200)
+        # userFile = filedialog.askopenfilename()
+        # -- Inicio do processamento da imagem -- #
+        userImg = cv.resize(cv.Canny(cv.imread(userFile), 125, 175), dim)
+        uContours, uHierarchies = cv.findContours(userImg, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+        #cv.imshow('Teste', userImg)
 
-userFile = filedialog.askopenfilename()
-
-# -- Inicio do processamento da imagem -- #
-userImg = cv.resize(cv.Canny(cv.imread(userFile), 125, 175), dim)
-uContours, uHierarchies = cv.findContours(userImg, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-cv.imshow('Teste', userImg)
-
-for files in os.listdir('database'):
-    f = os.path.join('database', files)
-    if os.path.isfile(f):
-        baseImg = cv.resize(cv.Canny(cv.imread(f), 125, 175), dim)
-        bContours, bHierarchies = cv.findContours(baseImg, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-        if len(uContours) == len(bContours):
-            print('Bem vindo!')
-            break
-        else:
-            print('Digital não encontrada no sistema!')
-
-cv.waitKey(0)
-
-
-
-
-
+        path = str(pathlib.Path(__file__).parent.resolve()) + '\\database'
+        
+        for files in os.listdir(path):
+            f = os.path.join(path, files)
+            if os.path.isfile(f):
+                baseImg = cv.resize(cv.Canny(cv.imread(f), 125, 175), dim)
+                bContours, bHierarchies = cv.findContours(baseImg, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+                if len(uContours) == len(bContours):
+                    #print('Bem vindo!')
+                    return True
+        return False
--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from kivy.uix.screenmanager import NoTransition, ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.uix.label import Label
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.filemanager import MDFileManager
 from kivy.utils import get_color_from_hex as C
 from kivy.lang import Builder
+from kivymd.toast import toast
+import pathlib
 import utils
+import OpenCV.read as OpenCV
 
 Window.size = (800, 600)
 
@@ -63,11 +67,19 @@ class TelaDeLogin(Screen):      #Tela de login       - Logar no sistema
     def build(self):
         pass
 
+    def select_path(self, path):
+        self.file.exit_manager(self.file)
+        if OpenCV.APIDigital().ProcurarDigital(path):
+            TelaPrincipal.switch_to(screens[4])
+            toast('Bem Vindo!')
+
     def login(self):
         global cliente, User
         login = self.ids.login.text
         if utils.validarEmail(login):
-            password = self.ids.password.text
+            #password = self.ids.password.text
+            self.file = fileChoose
+            self.file.escolherArquivo(fileChoose,self.select_path)
         else:
             self.ids.msgError.text = "Login invalid!"
 
@@ -76,20 +88,25 @@ class TelaDeLogin(Screen):      #Tela de login       - Logar no sistema
 
     def back(self):
         TelaPrincipal.switch_to(screens[0])
+        
 
 class TelaDeRegDigital(Screen): #Tela de importação das digitais - Importar as digitais
-    
     def build(self):
         pass
-
-    def import_digital(self):
-        ()
 
     def register_digital(self):
         ()
         
     def back(self):
-        TelaPrincipal.switch_to(screens[4])
+        TelaPrincipal.switch_to(screens[2])
+
+    def select_path(self, path):
+        self.file.exit_manager(self.file)
+        self.ids.fingerprint.text = path
+
+    def import_digital(self):
+        self.file = fileChoose
+        self.file.escolherArquivo(fileChoose,self.select_path)
 
 class TelaDePerfil(Screen):     #Tela de perfil     - Gerenciar as digitais
     
@@ -105,4 +122,17 @@ class MainApp(MDApp):
         TelaPrincipal.switch_to(screens[0])
         return TelaPrincipal
 
+class fileChoose():
+    def escolherArquivo(self, selectpath):
+
+        path = str(pathlib.Path(__file__).parent.resolve())  # path to the directory that will be opened in the file manager
+        self.file_manager = MDFileManager(
+            exit_manager = self.exit_manager,  # function called when the user reaches directory tree root
+            select_path = selectpath,  # function called when selecting a file/directory
+        )
+        self.file_manager.show(path)
+
+    def exit_manager(self, *args):
+        self.manager_open = False
+        self.file_manager.close()
 MainApp().run()
